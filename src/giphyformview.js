e900ef5d3441145823da00beba9758c9b3f9058b
Marek Lewandowski <code@mlewandowski.com>
2022-03-24 16:26:54 +0100

    Beginnings of text input in the dropdown.

---
+++ src/giphyformview.js
@@ -0,0 +1,224 @@
+/**
+ * @license Copyright (c) 2003-2022, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+/**
+ * @module giphy/ui/giphyformview
+ */
+
+import {
+	View,
+	ButtonView,
+	FormHeaderView,
+	LabeledFieldView,
+
+	Model,
+	FocusCycler,
+	createLabeledInputText,
+	ViewCollection,
+	injectCssTransitionDisabler,
+
+	createDropdown,
+	addListToDropdown
+} from 'ckeditor5/src/ui';
+
+import {
+	FocusTracker,
+	KeystrokeHandler,
+	Collection,
+	Rect,
+	isVisible
+} from 'ckeditor5/src/utils';
+
+// See: #8833.
+// eslint-disable-next-line ckeditor5-rules/ckeditor-imports
+import '@ckeditor/ckeditor5-ui/theme/components/responsive-form/responsiveform.css';
+import '../theme/giphyformview.css';
+
+// eslint-disable-next-line ckeditor5-rules/ckeditor-imports
+import previousArrow from '@ckeditor/ckeditor5-ui/theme/icons/previous-arrow.svg';
+import { icons } from 'ckeditor5/src/core';
+
+/**
+ * The find and replace form view class.
+ *
+ * See {@link module:giphy/giphyformview~GiphyFormView}.
+ *
+ * @extends module:ui/view~View
+ */
+export default class GiphyFormView extends View {
+	/**
+	 * Creates an instance of the {@link module:giphy/giphyformview~GiphyFormView} class.
+	 *
+	 * Also see {@link #render}.
+	 *
+	 * @param {module:utils/locale~Locale} [locale] The localization services instance.
+	 */
+	constructor( locale ) {
+		super( locale );
+
+		const t = locale.t;
+
+		/**
+		 * Tracks information about DOM focus in the form.
+		 *
+		 * @readonly
+		 * @member {module:utils/focustracker~FocusTracker}
+		 */
+		this.focusTracker = new FocusTracker();
+
+		/**
+		 * An instance of the {@link module:utils/keystrokehandler~KeystrokeHandler}.
+		 *
+		 * @readonly
+		 * @member {module:utils/keystrokehandler~KeystrokeHandler}
+		 */
+		this.keystrokes = new KeystrokeHandler();
+
+		/**
+		 * The URL input view.
+		 *
+		 * @member {module:ui/labeledfield/labeledfieldview~LabeledFieldView}
+		 */
+		this.filterInputView = this._createFilterInput();
+		/**
+		 * A collection of child views in the form.
+		 *
+		 * @readonly
+		 * @type {module:ui/viewcollection~ViewCollection}
+		 */
+		this.children = this._createFormChildren();
+
+		/**
+		 * A collection of views that can be focused in the form.
+		 *
+		 * @readonly
+		 * @protected
+		 * @member {module:ui/viewcollection~ViewCollection}
+		 */
+		this._focusables = new ViewCollection();
+
+		/**
+		 * Helps cycling over {@link #_focusables} in the form.
+		 *
+		 * @readonly
+		 * @protected
+		 * @member {module:ui/focuscycler~FocusCycler}
+		 */
+		this._focusCycler = new FocusCycler( {
+			focusables: this._focusables,
+			focusTracker: this.focusTracker,
+			keystrokeHandler: this.keystrokes,
+			actions: {
+				// Navigate form fields backwards using the Shift + Tab keystroke.
+				focusPrevious: 'shift + tab',
+
+				// Navigate form fields forwards using the Tab key.
+				focusNext: 'tab'
+			}
+		} );
+
+		this.setTemplate( {
+			tag: 'form',
+
+			attributes: {
+				class: [ 'ck', 'ck-giphy-form', 'ck-responsive-form' ],
+
+				// https://github.com/ckeditor/ckeditor5-link/issues/90
+				tabindex: '-1'
+			},
+
+			children: this.children
+		} );
+
+		injectCssTransitionDisabler( this );
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	render() {
+		super.render();
+
+		const childViews = [
+			this.filterInputView,
+			// @todo: grid view
+		];
+
+		childViews.forEach( v => {
+			// Register the view as focusable.
+			this._focusables.add( v );
+
+			// Register the view in the focus tracker.
+			this.focusTracker.add( v.element );
+		} );
+
+		// Start listening for the keystrokes coming from #element.
+		this.keystrokes.listenTo( this.element );
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	destroy() {
+		super.destroy();
+
+		this.focusTracker.destroy();
+		this.keystrokes.destroy();
+	}
+
+	/**
+	 * Focuses the fist {@link #_focusables} in the form.
+	 */
+	focus() {
+		this._focusCycler.focusFirst();
+	}
+
+	/**
+	 * Creates a labeled input view.
+	 *
+	 * @private
+	 * @returns {module:ui/labeledfield/labeledfieldview~LabeledFieldView} Labeled field view instance.
+	 */
+	_createFilterInput() {
+		const t = this.locale.t;
+		const labeledInput = new LabeledFieldView( this.locale, createLabeledInputText );
+
+		labeledInput.label = t( 'What you\'re looking for?' );
+
+		return labeledInput;
+	}
+
+	/**
+	 * Populates the {@link #children} collection of the form.
+	 *
+	 * @private
+	 * @param {module:utils/collection~Collection} manualDecorators A reference to
+	 * @returns {module:ui/viewcollection~ViewCollection} The children of link form view.
+	 */
+	_createFormChildren() {
+		const children = this.createCollection();
+
+		children.add( this.filterInputView );
+
+		// @todo: children.add( this.gridView );
+
+		return children;
+	}
+}
+
+// @todo: verify events
+
+/**
+ * Fired when the form view is submitted (when one of the children triggered the submit event),
+ * for example with a click on {@link #saveButtonView}.
+ *
+ * @event submit
+ */
+
+/**
+ * Fired when the form view is canceled, for example with a click on {@link #cancelButtonView}.
+ *
+ * @event cancel
+ */
--- src/giphyui.js
+++ src/giphyui.js
@@ -1,8 +1,10 @@
 import { Plugin } from 'ckeditor5/src/core';
-import { ButtonView } from 'ckeditor5/src/ui';
+import { createDropdown } from 'ckeditor5/src/ui';
 
 import GiphyIntegration from './giphyintegration';
 import giphyIcon from '../theme/icons/giphy.svg';
+import GiphyFormView from './giphyformview';
+
 
 export default class GiphyUI extends Plugin {
 	static get pluginName() {
@@ -20,22 +22,26 @@ export default class GiphyUI extends Plugin {
 
 		// Add the "giphy" button to feature components.
 		editor.ui.componentFactory.add( 'giphy', locale => {
-			const view = new ButtonView( locale );
+			const dropdownView = createDropdown( locale );
+			const formView = this.formView = new GiphyFormView( locale );
 
-			view.set( {
+			dropdownView.panelView.children.add( formView );
+
+			dropdownView.buttonView.set( {
 				label: t( 'Giphy' ),
 				icon: giphyIcon,
 				tooltip: true
 			} );
 
-			// Insert a text into the editor after clicking the button.
-			this.listenTo( view, 'execute', async () => {
-				const gifs = await editor.plugins.get('GiphyIntegration').getGifs().then(response => this._handleResponse(response));
-				console.log('gifs', gifs);
+			dropdownView.on( 'change:isOpen', async () => {
+				const gifs = await editor.plugins.get( 'GiphyIntegration' ).getGifs()
+					.then( response => this._handleResponse( response ) );
+
+				console.log( 'gifs', gifs );
 				editor.editing.view.focus();
 			} );
 
-			return view;
+			return dropdownView;
 		} );
 	}
 
